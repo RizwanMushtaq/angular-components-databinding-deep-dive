15322f276cdbeecc07fe0483932f566ee5e0997b
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-03-26 08:56:27 +0100

    learning angular property and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { ServerElement } from './serverElement.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'components-databinding-deep-dive';
+
+  serverElements: ServerElement[] = [
+    {
+      type: 'server',
+      name: 'test server',
+      content: 'Just a test',
+    },
+  ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBluePrintAdded(bluePrintData: {
+    bluePrintName: string;
+    bluePrintContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.bluePrintName,
+      content: bluePrintData.bluePrintContent,
+    });
+  }
 }
